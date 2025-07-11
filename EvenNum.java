2d7dc0f9545e35347a8d13cb8326a11ce71a6108
JOSWIN SHAJU <joswinshaju2028@cs.sjcetpalai.ac.in>
2025-07-11 10:35:43 +0530

    Add files via upload

---
+++ EvenNum.java
@@ -0,0 +1,25 @@
+/* 
+ * Title: Even num upto n
+ * Author : Joswin Shaju
+ * Date : 11/07/2025
+ */
+
+package prgms;
+
+import java.util.Scanner;
+
+public class EvenNum {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the limit: ");
+		int limit=sc.nextInt();
+		int i;
+		for(i=2;i<limit;i+=2) {
+			
+			System.out.println("no are: " +i);
+		}
+		
+		sc.close();		
+	}
+
+}
