715dab7e392405497b3f3eca02128ea656b275b0
JOSWIN SHAJU <joswinshaju2028@cs.sjcetpalai.ac.in>
2025-07-29 12:45:28 +0530

    Add files via upload

---
+++ StaticDemo.java
@@ -0,0 +1,30 @@
+/* 
+ * Title: Book Detail
+ * Author : Joswin Shaju
+ * Date : 29/07/2025
+ */
+
+package jv;
+
+class Student{
+	String name;
+	static int StudentCount=0;
+	Student( String name){
+		this.name=name;
+		StudentCount++;
+	}
+	public void printDetails() {
+		System.out.println(name);
+		System.out.println(StudentCount);
+	}
+}
+
+public class StaticDemo {
+	public static void main(String [] args) {
+		Student student=new Student("test1");
+		student.printDetails();
+		Student student1=new Student("test2");
+		student1.printDetails();
+		
+	}
+}
